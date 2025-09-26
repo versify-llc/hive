c451b8ae65ee38d14be3d272e414ffc10b726285
Richie Tarkowski <tarkowr@gmail.com>
2025-09-25 20:14:58 -0500

    hive_flutter package upgrades

--- hive_flutter/lib/hive_flutter.dart
+++ hive_flutter/lib/hive_flutter.dart
@@ -6,7 +6,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hive/hive.dart';
 import 'package:path/path.dart'
-    if (dart.library.js_interop) 'src/stub/path.dart' as path_helper;
+    if (dart.library.js_interop) 'src/stub/path.dart'
+    as path_helper;
 import 'package:path_provider/path_provider.dart'
     if (dart.library.js_interop) 'src/stub/path_provider.dart';
 
@@ -14,4 +15,3 @@ export 'package:hive/hive.dart';
 
 part 'src/box_extensions.dart';
 part 'src/hive_extensions.dart';
-part 'src/watch_box_builder.dart';
